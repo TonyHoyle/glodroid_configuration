286b0f60d44bf99f9dd171428be51f0e48230a1d
Michał Gapiński <mike@gapinski.eu>
2024-02-06 20:49:59 +0100

    Revert "RPI3: High performance screen capture"
    This reverts commit b63b558b3f4e927dea1bda3ef3c229c158893e1a.

--- common/codecs/board.mk
+++ common/codecs/board.mk
@@ -19,4 +19,3 @@ BOARD_VENDOR_SEPOLICY_DIRS += $(BCDC_PATH)/sepolicy/vendor
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(BCDC_PATH)/device_framework_compatibility_matrix_ffmpegcodec2.xml \
-    $(BCDC_PATH)/device_framework_compatibility_matrix_v4l2codec2.xml \
--- common/codecs/device.mk
+++ common/codecs/device.mk
@@ -4,8 +4,6 @@
 #
 # Copyright (C) 2022 Roman Stratiienko (r.stratiienko@gmail.com)
 
-PRODUCT_SOONG_NAMESPACES += glodroid/vendor/v4l2_codec2
-
 PRODUCT_PACKAGES += \
     android.hardware.media.c2@1.0-service \
 
@@ -27,7 +25,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
     $(LOCAL_PATH)/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
-    $(LOCAL_PATH)/media_codecs_v4l2_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_v4l2_c2.xml \
 
 # Vendor seccomp policy files:
 PRODUCT_COPY_FILES += \
@@ -37,23 +34,16 @@ PRODUCT_COPY_FILES += \
 # Codec2.0 poolMask:
 #   ION(16)
 #   GRALLOC(17)
-#   GRALLOC/BUFFERQUEUE(18)
-#   GRALLOC/BLOB(19)
+#   BUFFERQUEUE(18)
+#   BLOB(19)
 #   V4L2_BUFFERQUEUE(20)
 #   V4L2_BUFFERPOOL(21)
 #   SECURE_LINEAR(22)
 #   SECURE_GRAPHIC(23)
-# For HW codecs we need GRALLOC/BQ + GRALLOC/BLOB.
-# When all are selected, it looks like the correct ones are used.
+#
+# For linear buffer allocation:
+#   If ION is chosen, then the mask should be 0xf50000
+#   If GRALLOC is chosen, then the mask should be 0xf60000
+#   If BLOB is chosen, then the mask should be 0xfc0000
 PRODUCT_VENDOR_PROPERTIES += \
-    debug.stagefright.c2-poolmask=0xffffff \
-
-# V4L2 codec2
-PRODUCT_PACKAGES += \
-    android.hardware.media.c2@1.0-service-v4l2 \
-    libv4l2_codec2_vendor_allocator            \
-    libc2plugin_store                          \
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.v4l2_codec2.decode_concurrent_instances=8 \
-    ro.vendor.v4l2_codec2.encode_concurrent_instances=8 \
+    debug.stagefright.c2-poolmask=0x0c0000 \
